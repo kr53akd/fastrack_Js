e36589f8a0b14d74e5920e39f594a967d6e284e9
kr53akd <akd.kr53@gmail.com>
2025-07-24 10:51:38 +0530

    optimzed the createNewElement function

--- js/component/navbar.js
+++ js/component/navbar.js
@@ -2,16 +2,17 @@ import { createNewElement } from "../lib.js"
 
 export function navbar(){
     const header = createNewElement("header");
+    
     //animation area of text scrolling from left to right
-    const animatonHeading = header.appendChild(createNewElement("nav",[{id:"animation-block"},{class:'w-full h-35'}]))
+    const animatonHeading = header.appendChild(createNewElement("nav",{id:"animation-block", class:'w-full h-35'}))
 
     //Navbar main contents
-    const nav = header?.appendChild(createNewElement("nav", [{id:"navbar"}]));
-    const logo = nav.appendChild(createNewElement("div", [{id:"logo"},{class:"flex w-full"}]));
-    logo.appendChild(createNewElement("img", [{id:"logo-img"},{src:"/public/FASTRACK-Logo-Black.png"}, {alt:"logo space"}, {class:"logo"}]))
+    const nav = header?.appendChild(createNewElement("nav", {id:"navbar"}));
+    const logo = nav.appendChild(createNewElement("div", {id:"logo", class:"flex w-full"}));
+    logo.appendChild(createNewElement("img", {id:"logo-img", src:"/public/FASTRACK-Logo-Black.png", alt:"logo space", class:"logo", onclick:"console.log('clicked on the logo')"}))
   
     //location area
-    const locationAria = header.appendChild(createNewElement("nav", [{id:"location-aria"}, {class:"text-sm flex items-center"}]));
+    const locationAria = header.appendChild(createNewElement("nav", {id:"location-aria", class:"text-sm flex items-center"}));
     const locationPara = locationAria.appendChild(createNewElement("p"));
     locationPara.innerHTML = "Select the location to check product availability"
   return header;
--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,8 @@
 export function createNewElement(elementName, attributes){
     const tag = document.createElement(elementName);
-
-    attributes?.forEach((element)=>{
-        console.log(Object.keys(element)[0], element[Object?.keys(element)[0]], 6)
-        tag?.setAttribute(Object.keys(element)[0], element[Object?.keys(element)[0]]);
+    
+    attributes && Object?.keys(attributes)?.forEach((element)=>{
+        tag?.setAttribute(element, attributes[element]);
     })
     return tag;
 }
