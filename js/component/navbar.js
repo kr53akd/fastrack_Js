48b363b499527fab4fcabebfca31ba8084475c46
kr53akd <akd.kr53@gmail.com>
2025-07-25 14:57:37 +0530

    fixed the aligment and overflow issues

--- js/component/navbar.js
+++ js/component/navbar.js
@@ -1,39 +1,59 @@
 import { createNewElement } from "../lib.js"
 
 export function navbar(){
+
+    const listItems = [
+      {
+        name:"WATCHES",
+      },
+      {
+        name:"SMART WATCHES",
+      },
+      {
+        name:"VYB BY FASTRACK",
+      },
+      {
+        name:"SALE",
+      },
+      {
+        name:"GIFTING",
+      },
+      {
+        name:"ACCESSORIES",
+      },
+      {
+        name:"MORE",
+      },
+    ]
     const header = createNewElement("header");
     
     //animation area of text scrolling from left to right
     const animatonHeading = header.appendChild(createNewElement("nav",{id:"animation-block", class:'w-full h-35'}))
 
     //Navbar main contents
-    const nav = header?.appendChild(createNewElement("nav", {id:"navbar"}));
+    const nav = header?.appendChild(createNewElement("nav", {id:"navbar", class:"w-full"}));
 
-
-    const logo = nav.appendChild(createNewElement("div", {id:"logo", class:"flex w-full"}));
+    const container = nav.appendChild(createNewElement("div", {class:"container"}));
+    const logo = container.appendChild(createNewElement("div", {id:"logo"}));
     logo.appendChild(createNewElement("img", {id:"logo-img", src:"/public/FASTRACK-Logo-Black.png", alt:"logo space", class:"logo", onclick:"console.log('clicked on the logo')"}))
 
-      const div_item = nav.appendChild(createNewElement("div",{class:"list-item"}))
-    const list = div_item.appendChild(createNewElement("ul"))
-    const a=list.appendChild(createNewElement("li" ))
-   const b=  list.appendChild(createNewElement("li" ))
-    const c=list.appendChild(createNewElement("li" ))
-    const d=list.appendChild(createNewElement("li" ))
-    const e=list.appendChild(createNewElement("li" ))
-    const f=list.appendChild(createNewElement("li" ))
-    const g=list.appendChild(createNewElement("li" ))
-
-    a.innerHTML="WATCHES"
-    b.innerHTML="SMART WATCHES"
-    c.innerHTML="VYB BY FASTRACK"
-    d.innerHTML="SALE"
-    e.innerHTML="GIFTING"
-    f.innerHTML="ACCESSORIES"
-    g.innerHTML="MORE"
+    const unOrderedList = container.appendChild(createNewElement("ul"));
+    
+    listItems?.map((object, index)=>{
+      const list = unOrderedList?.appendChild(createNewElement("li"));
+      list.textContent = object.name;
+    })
   
+    const actionIcons = container.appendChild(createNewElement("div", {class:"nav-icon"}))
+    const para = actionIcons.appendChild(createNewElement("p"));
+    para.innerHTML = "hello"
+
     //location area
     const locationAria = header.appendChild(createNewElement("nav", {id:"location-aria", class:"text-sm flex items-center"}));
-    const locationPara = locationAria.appendChild(createNewElement("p"));
-    locationPara.innerHTML = "Select a location to check product availability"
+    const locationPara = locationAria.appendChild(createNewElement("p",{class:"para-location-gap"}));
+    locationPara.innerHTML = "Select a location to check product availability "
+    const anchor = locationPara.appendChild(createNewElement("a", {href:"/location", class:""}));
+    anchor.textContent = "Add Location";
+    
   return header;
 }
--- js/lib.js
+++ js/lib.js
@@ -1,9 +1,10 @@
 export function createNewElement(elementName, attributes){
     const tag = document.createElement(elementName);
-    
-    attributes && Object?.keys(attributes)?.forEach((element)=>{
-        tag?.setAttribute(element, attributes[element]);
-    })
+
+    attributes && Object?.keys(attributes)?.forEach((key)=>{
+        tag?.setAttribute(key, attributes[key]);
+    });
+
     return tag;
 }
 
